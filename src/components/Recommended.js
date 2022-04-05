aedff430800572d86f5eeae9be7e6b9477dc74f0
Svetlana Stoycheva <s.stoycheva@gmail.com>
2022-04-05 17:28:58 +0300

    Recommendend component in all pages;

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import data from '.././data';
 import { BiBookmark } from 'react-icons/bi';
 import { MdLocalMovies } from 'react-icons/md';
 import { FaBookmark } from 'react-icons/fa';
 
 const Recommended = () => {
-  const recommendedItems = data.filter((i) => !i.isTrending);
+  const [recommendedItems, setRecommendedItems] = useState(
+    data.filter((i) => !i.isTrending)
+  );
 
+  //depending on the page: home, movies or series, show the correspondent recommendedItems
+  const currentPath = window.location.pathname;
+
+  useEffect(() => {
+    const recommendedData = data.filter((i) => !i.isTrending);
+    if (currentPath === '/') {
+      setRecommendedItems(recommendedData);
+    } else if (currentPath === '/movies') {
+      const movieData = recommendedData.filter((i) => i.category === 'Movie');
+      setRecommendedItems(movieData);
+    } else if (currentPath === '/tvseries') {
+      const seriesData = recommendedData.filter(
+        (i) => i.category === 'TV Series'
+      );
+      setRecommendedItems(seriesData);
+    }
+  }, [currentPath]);
   return (
     <section className='recommended-container'>
       <h4 className='slider-title'>Recommended for you</h4>
--- src/pages/TVSeriesPage.js
+++ src/pages/TVSeriesPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import SearchForm from '../components/SearchForm';
+import Recommended from '../components/Recommended';
 
 const TVSeriesPage = () => {
   return (
@@ -8,7 +9,9 @@ const TVSeriesPage = () => {
       <Navbar />
       <div className='home-page-inner-container'>
         <SearchForm />
-        <div className='home-page-content-container'></div>
+        <div className='movie-page-content-container'>
+          <Recommended />
+        </div>
       </div>
     </section>
   );
